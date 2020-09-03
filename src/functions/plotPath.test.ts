eb42306a8621dc71e4317757f807a79037b1d966
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-09-03 19:36:44 -0400

    Tests complete

--- src/functions/plotPath.test.ts
+++ src/functions/plotPath.test.ts
@@ -27,3 +27,23 @@ test('returns array of 5 coordinates', ()=> {
         {x: 2, y: 2, z: 'N'},
     ]);
 });
+
+test('original test input 1', ()=> {
+    const params = {
+        instructions: 'LMLMLMLMM',
+        startX: 1,
+        startY: 2,
+        startZ: 'N'
+    }
+    expect(plotPath(params)[5]).toEqual({x: 1, y: 3, z: 'N'});
+});
+
+test('original test input 2', ()=> {
+    const params = {
+        instructions: 'MMRMMRMRRM',
+        startX: 3,
+        startY: 3,
+        startZ: 'E'
+    }
+    expect(plotPath(params)[6]).toEqual({x: 5, y: 1, z: 'E'});
+});
---
+++ src/functions/willCollide.test.ts
@@ -0,0 +1,27 @@
+var {willCollide} = require('./willCollide');
+
+test('returns true when coordinate blocks path', ()=> {
+    const params = {
+        path: [
+            {x: 1, y: 1, z: 'N'},
+            {x: 1, y: 2, z: 'N'},
+            {x: 2, y: 2, z: 'E'}
+        ],
+        x: 1,
+        y: 2
+    }
+    expect(willCollide(params)).toBe(true);
+});
+
+test('returns false when coordinate does not block path', ()=> {
+    const params = {
+        path: [
+            {x: 1, y: 1, z: 'N'},
+            {x: 1, y: 2, z: 'N'},
+            {x: 2, y: 2, z: 'E'}
+        ],
+        x: 1,
+        y: 3
+    }
+    expect(willCollide(params)).toBe(false);
+});
