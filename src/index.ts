3e00d48a58c4732df2da3e13dc6d754ac6ec7cb0
pavloskaralis <52301933+pavloskaralis@users.noreply.github.com>
2020-09-04 00:58:23 -0400

    converted flow to class

--- src/index.ts
+++ src/index.ts
@@ -10,211 +10,203 @@ var {isInBounds} = require('./functions/isInBounds');
 var {plotPath} = require('./functions/plotPath');
 var {willCollide} = require('./functions/willCollide');
 
-//outputs
 type Coordinate = {
     x: number,
     y: number,
     z: string
-}
-let lastA: Coordinate;
-let lastB: Coordinate; 
+} 
 
-//variables
-let gridboundsX: number;
-let gridboundsY: number; 
-let startAX: number;
-let startAY: number;
-let startAZ: string;
-let startBX: number;
-let startBY: number; 
-let startBZ: string;
-let coordinatesA: Array<Coordinate>;
-let coordinatesB: Array<Coordinate>;
-
-//first question 
-function requestGridbounds() {    
-    inquirer.prompt(questions.GRIDBOUNDS).then(({gridbounds})=>{
+class Driver {
+    lastA : Coordinate;
+    lastB: Coordinate
+    gridboundsX: number;
+    gridboundsY: number; 
+    startAX: number;
+    startAY: number;
+    startAZ: string;
+    startBX: number;
+    startBY: number; 
+    startBZ: string;
+    coordinatesA: Array<Coordinate>;
+    coordinatesB: Array<Coordinate>;
+    
+    async requestGridbounds() {    
+        const {gridbounds} = await inquirer.prompt(questions.GRIDBOUNDS)
         //check if valid input
         if(!isValid.gridboundsInput(gridbounds)) {
             consola.info('Error: grid bounds must contain two integers.');
-            return requestGridbounds(); 
+            return this.requestGridbounds(); 
         }
         //break gridbounds into 2 numbers 
         const splitGBs = gridbounds.split(' ');
-        gridboundsX = parseInt(splitGBs[0]);
-        gridboundsY = parseInt(splitGBs[1]);
-        //prompt second question
-        return requestStartA(); 
-    })
-}
+        this.gridboundsX = parseInt(splitGBs[0]);
+        this.gridboundsY = parseInt(splitGBs[1]); 
+        return;
+    }
 
-//second question
-function requestStartA () {
-    inquirer.prompt(questions.START_A).then(({startA})=>{
+    async requestStartA () {
+        const {startA} = await inquirer.prompt(questions.START_A)
         //check if valid input
         if(!isValid.startInput(startA)) {
             consola.info('Error: starting position must contain two intergers and a heading (N, E, S, or W).');
-            return requestStartA(); 
+            return this.requestStartA(); 
         }
         //break startA into 2 numbers and string heading 
         const splitStartA = startA.split(' ');
-        startAX = parseFloat(splitStartA[0]);
-        startAY = parseFloat(splitStartA[1]);
-        startAZ = splitStartA[2];
+        this.startAX = parseFloat(splitStartA[0]);
+        this.startAY = parseFloat(splitStartA[1]);
+        this.startAZ = splitStartA[2];
         //check if startA is in bounds
         const iIBParams = {
-            x: startAX,
-            y: startAY,
-            gridboundsX,
-            gridboundsY
+            x: this.startAX,
+            y: this.startAY,
+            gridboundsX: this.gridboundsX,
+            gridboundsY: this.gridboundsY
         }
         if(!isInBounds(iIBParams)){
             consola.info('Error: starting coordinates must exist within grid bounds.');
-            return requestStartA(); 
-        }
-        //prompt third question
-        requestInstructionsA();
-    })  
-}
+            return this.requestStartA(); 
+        }      
+        return;
+    }
 
-//third question 
-function requestInstructionsA () {
-    inquirer.prompt(questions.INSTRUCTIONS_A).then(({instructionsA})=>{
+    async requestInstructionsA () {
+        const {instructionsA} = await inquirer.prompt(questions.INSTRUCTIONS_A);
         //check if valid input
         if(!isValid.instructionsInput(instructionsA)) {
             consola.info('Error: instructions must only use characters L, R, and M (left, right, and move).');
-            return requestInstructionsA(); 
+            return this.requestInstructionsA(); 
         }
         //retrieve roverA coordinates
         const pPParams = {
             instructions: instructionsA,
-            startX: startAX,
-            startY: startAY,
-            startZ: startAZ
+            startX: this.startAX,
+            startY: this.startAY,
+            startZ: this.startAZ
         }
-        coordinatesA = plotPath(pPParams);
+        this.coordinatesA = plotPath(pPParams);
         //check if path stays in bounds
-        if(coordinatesA.some(obj => {
+        if(this.coordinatesA.some(obj => {
             const iIBParams = {
                 x: obj.x,
                 y: obj.y,
-                gridboundsX,
-                gridboundsY
+                gridboundsX: this.gridboundsX,
+                gridboundsY: this.gridboundsY
             }
             return !isInBounds(iIBParams);
         })) {
             consola.info('Error: instructions result in rover A leaving grid bounds.');
-            return requestInstructionsA(); 
+            return this.requestInstructionsA(); 
         }
         //store first output
-        lastA = coordinatesA[coordinatesA.length - 1];
-        //prompt 4th question 
-        requestStartB();
-    })  
-}
+        this.lastA = this.coordinatesA[this.coordinatesA.length - 1];
+        return;
+    }
 
-//fourth question 
-function requestStartB () {
-    inquirer.prompt(questions.START_B).then(({startB})=>{
+    async requestStartB () {
+        const{startB} = await inquirer.prompt(questions.START_B)
         //check if valid input
         if(!isValid.startInput(startB)) {
             consola.info('Error: starting position must contain two intergers and a heading (N, E, S, or W).');
-            return requestStartB(); 
+            return this.requestStartB(); 
         }
         //break startB into 2 numbers and string heading 
         const splitStartB = startB.split(' ');
-        startBX = parseFloat(splitStartB[0]);
-        startBY = parseFloat(splitStartB[1]);
-        startBZ = splitStartB[2];
+        this.startBX = parseFloat(splitStartB[0]);
+        this.startBY = parseFloat(splitStartB[1]);
+        this.startBZ = splitStartB[2];
         //check if startB is in bounds
         const iIBParams = {
-            x: startBX,
-            y: startBY,
-            gridboundsX,
-            gridboundsY
+            x: this.startBX,
+            y: this.startBY,
+            gridboundsX: this. gridboundsX,
+            gridboundsY: this.gridboundsY
         }
         if(!isInBounds(iIBParams)){
             consola.info('Error: starting coordinates must exist within grid bounds.');
-            return requestStartB(); 
+            return this.requestStartB(); 
         }
         //check if rover B start is in path of rover A
         const wCParams = {
-            path: coordinatesA,
-            x: startBX,
-            y: startBY
+            path: this.coordinatesA,
+            x: this.startBX,
+            y: this.startBY
         }
         if(willCollide(wCParams)){
             consola.info('Error: rover B starting coordinates place it in rover A path.');
-            return requestStartB(); 
+            return this.requestStartB(); 
         }
-        //prompt 5th question
-        requestInstructionsB();
-    })  
-}
-
-//fifth question
-function requestInstructionsB () {
-    inquirer.prompt(questions.INSTRUCTIONS_B).then(({instructionsB})=>{
+        return;
+    }
+   
+    async requestInstructionsB () {
+        const {instructionsB} = await inquirer.prompt(questions.INSTRUCTIONS_B);
         //check if valid input
         if(!isValid.instructionsInput(instructionsB)) {
             consola.info('Error: instructions must only use characters L, R, and M (left, right, and move).');
-            return requestInstructionsB(); 
+            return this.requestInstructionsB(); 
         }
         //retrieve roverB coordinates
         const pParams = {
             instructions: instructionsB,
-            startX: startBX,
-            startY: startBY,
-            startZ: startBZ
+            startX: this.startBX,
+            startY: this.startBY,
+            startZ: this.startBZ
         }
-        coordinatesB = plotPath(pParams);
+        this.coordinatesB = plotPath(pParams);
         //check if path stays in bounds
-        if(coordinatesB.some(obj => {
+        if(this.coordinatesB.some(obj => {
             const iIBParams = {
                 x: obj.x,
                 y: obj.y,
-                gridboundsX,
-                gridboundsY
+                gridboundsX: this.gridboundsX,
+                gridboundsY: this.gridboundsY
             }
             return !isInBounds(iIBParams);
         })) {
             consola.info('Error: instructions result in rover B leaving grid bounds.');
-            return requestInstructionsB(); 
+            return this.requestInstructionsB(); 
         }
         //check if rover A end is in path of rover B
         const wCParams = {
-            path: coordinatesB,
-            x: lastA.x,
-            y: lastA.y
+            path: this.coordinatesB,
+            x: this.lastA.x,
+            y: this.lastA.y
         }
         if(willCollide(wCParams)){
             consola.info('Error: instructions result in rover B colliding with rover A ending coordinates.');
-            return requestInstructionsB(); 
+            return this.requestInstructionsB(); 
         }
         //store second output
-        lastB = coordinatesB[coordinatesB.length - 1];
+        this.lastB = this.coordinatesB[this.coordinatesB.length - 1];
         //prompt final question
-        console.log(chalk.red('Rover A Ending Positon: ') + `${lastA.x} ${lastA.y} ${lastA.z}`);
-        console.log(chalk.red('Rover B Ending Positon: ') + `${lastB.x} ${lastB.y} ${lastB.z}`);
-        endCLI();
-    })  
-}
-
-//final question
-function endCLI() {
-    inquirer.prompt(questions.RESTART).then(({restart})=>{
+        console.log(chalk.red('Rover A Ending Positon: ') + `${this.lastA.x} ${this.lastA.y} ${this.lastA.z}`);
+        console.log(chalk.red('Rover B Ending Positon: ') + `${this.lastB.x} ${this.lastB.y} ${this.lastB.z}`);
+        return;
+    }
+     
+    async endCLI() {
+        const {restart} = await inquirer.prompt(questions.RESTART);
         if(!isValid.restartInput(restart)) {
             consola.info('Error: please provide yes or no answer.');
-            return endCLI(); 
+            return this.endCLI(); 
         }
-        restart === 'yes' ? requestGridbounds() : consola.info('Thank you for using Mars Rover.')
-    })
+        return restart === 'yes' ? this.startCLI() : consola.info('Thank you for using Mars Rover.');    
+    }
+
+    async startCLI() {
+       await this.requestGridbounds();
+       await this.requestStartA();
+       await this.requestInstructionsA();
+       await this.requestStartB();
+       await this.requestInstructionsB();
+       await this.endCLI();
+    }
 }
 
 //start prompt
 clear();
 console.log(chalk.red(figlet.textSync('Mars Rover', { horizontalLayout: 'full' })))
 consola.info('Welcome to Mars Rover, a TypeScript CLI for instructing rovers on Mars.');
-requestGridbounds();
-
+const driver = new Driver().startCLI(); 
 
